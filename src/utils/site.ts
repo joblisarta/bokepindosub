95b7c53a699f928ebc91aa432d4bec92d182b2a5
joblisarta <166005441+joblisarta@users.noreply.github.com>
2025-06-28 19:40:22 +0700

    Add files via upload

--- src/utils/site.ts
+++ src/utils/site.ts
@@ -1,4 +1,4 @@
 export const nama = 'BokepIndoSub';
-export const desk = 'Video bokep indonesia jepang viral ngentot colmek bocil sampai tante.';
-export const url = 'https://bokepindosub.pages.dev';
-export const terbit = '2025-06-23T00:00:00Z';
+export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Viral Sma Cindo Tante China';
+export const url = 'https://bokepindokorea.pages.dev';
+export const terbit = '2025-06-23T00:01:03Z';
